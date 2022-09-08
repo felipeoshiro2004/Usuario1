293553ced1e2504eed289c6c2adc8ee08d388c03
felipeoshiro2004 <felipe.paula47@etec.sp.gov.br>
2022-09-08 19:51:42 -0300

    Initial commit

---
+++ MyQuizApplication-master/app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "O aplicativo Acertou corretamente?",
+            "O aplicativo Satisfez suas demandas?",
+            "Qual sua Avaliação sobre o Aplicativo?",
+
+    };
+
+    public static String choices[][] = {
+            {"Sim","Não","Um Pouco","O Aplicativo Falhou"},
+            {"Sim","Não","Um Pouco","Precisa Melhorar"},
+            {"Exelente","bom","ruim","pessimo"},
+
+    };
+
+    public static String correctAnswers[] = {
+            "Sim","Não","Um Pouco","O Aplicativo Falhou",
+            "Sim","Não","Um Pouco","Precisa Melhorar",
+            "Exelente","bom","ruim","pessimo",
+    };
+
+}
